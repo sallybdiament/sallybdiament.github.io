53af7d225a3080b921d8ba8d17fa31371e52e64b
Sally Diament <sally.barzilai@gmail.com>
2022-04-17 01:50:17 -0300

    tentando corrigir o alert novamente

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,6 @@
 window.onload = inConstruction
 
 function inConstruction() {
-    alert("Esta página está sendo construída em HTML, CSS e Javascript. Esta página está sendo construída em HTML, CSS e Javascript. Estou constantemente melhorando e corrigindo a página.");
+    alert("Esta página está sendo construída em HTML, CSS e Javascript. Estou constantemente melhorando e corrigindo a página.");
 
 }
